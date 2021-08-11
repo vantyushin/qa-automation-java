fb10b2b6e96fe77f60171bc96be7d0ea38e3199f
vantyushin <v.antyushin@tinkoff.ru>
2021-08-11 19:18:53 +0300

    Генерация и запуск mvn-проекта

---
+++ app/src/main/java/com/tinkoff/edu/App.java
@@ -0,0 +1,13 @@
+package com.tinkoff.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
