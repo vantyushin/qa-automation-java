fe12c08cf88ab7460be6a68e881e702cf9fa5ee5
vantyushin <v.antyushin@tinkoff.ru>
2021-08-12 10:06:56 +0300

    Апдейт №2 mvn-проекта

--- app/src/main/java/com/tinkoff/edu/App.java
+++ app/src/main/java/com/tinkoff/edu/App.java
@@ -5,8 +5,7 @@ package com.tinkoff.edu;
  *
  */
 public class App {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
